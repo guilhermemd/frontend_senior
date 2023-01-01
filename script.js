4698ead44b8a6e46a96f3a1e8b786a7ef537973c
guilherme <guilherme.md@gmail.com>
2023-01-01 18:58:13 -0300

    final commit happy new year

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+document.onmousemove = (event) => {
+  let body = document.querySelector("body");
+  // body.style.background = "red";
+  let snow = document.createElement("span");
+  let x = event.pageX;
+  let y = event.pageY;
+  let size = Math.random() * 50;
 
+  // snow.style.stroke = "red";
+  // snow.style.background = "blue";
+
+  snow.style.left = x + "px";
+  snow.style.top = y + "px";
+  snow.style.width = size + "px";
+  snow.style.height = size + "px";
+
+  body.appendChild(snow);
+
+  setTimeout(() => {
+    snow.remove();
+  }, 2000);
+};
